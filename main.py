a621a31ea7f3c835d89c45fa377fceaffe9ef700
Phoebe Young <pyou182@aucklanduni.ac.nz>
2024-09-24 10:52:06 +1200

    Draft function that creates labels

--- main.py
+++ main.py
@@ -11,6 +11,34 @@ window.title("Pokedex")
 window.config(padx=10, pady=10)
 
 # Write a function that creates labels
+def create_label(tk_interface, text, padx, pady, font=True, fontsize):
+
+    # Initialise label
+    if text != None:
+        label = tk.Label(tk_interface, text=text)
+    else:
+        label = tk.Label(tk_interface)
+
+    if font is True:
+        label.config(font=("Arial", fontsize))
+
+    label.pack(padx=padx, pady=pady)
+
+    return label
+
+
+tite_label = create_label(window, text="Phoebe's Pokedex", padx=10, pady=10, font=True, fontsize=32)
+
+pokemon_image = tk.Label(window)
+pokemon_image.pack(padx=30, pady=30)
+
+pokemon_information = tk.Label(window)
+pokemon_information.config(font=("Arial", 20))
+pokemon_information.pack(padx=10, pady=10)
+
+pokemon_types = tk.Label(window)
+pokemon_types.config(font=("Arial", 20))
+pokemon_types.pack(padx=10, pady=10)
 
 # Write function that finds and displays information on the pokemon given by the user window input
 
