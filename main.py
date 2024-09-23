39b86c05779d6810b84470d2462ecb4b2beace4b
Phoebe Young <pyou182@aucklanduni.ac.nz>
2024-09-24 08:54:11 +1200

    Initialise pokedex window

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ import tkinter as tk
 import urllib3
 from io import BytesIO
 
-pokemon = pkd.get(name='charmander')
+# Create a user interface window
+window = tk.Tk()
+window.geometry("600x500")
+window.title("Pokedex")
+window.config(padx=10, pady=10)
 
-print(pokemon.dex)
-print(pokemon.name)
-print(pokemon.abilities)
-print(pokemon.types)
+# Write a function that creates labels
+
+# Write function that finds and displays information on the pokemon given by the user window input
+
+window.mainloop()
